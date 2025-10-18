bc6a45646b79776bcfe60a057d22392e4990e92e
Jose Castro <50778346+JoseHCastro@users.noreply.github.com>
2025-10-18 12:09:45 -0400

    TopBar Widget test

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -6,6 +6,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text('Home Screen'),
+      ),
+    );
+     
   }
 }
